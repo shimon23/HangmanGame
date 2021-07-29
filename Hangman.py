3032918ad42a525ffd33d6cb5193665e21b51798
Shimon Hagag <shimonhagag@h-MacBook-Air-sl-Shimon.local>
2021-07-29 09:40:52 +0300

    Initial commit

---
+++ Hangman.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    print("""Welcome to the game Hangman      _    _
+   | |  | |
+   | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __
+   |  __  |/ _' | '_ \ / _' | '_ ' _ \ / _' | '_ \\
+   | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+   |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                        __/ |
+                       |___/""")
+
+
---
+++ practice.py
@@ -0,0 +1,28 @@
+
+
+
+
+def swap(a ,b):
+    a ,b = b, a
+    return a ,b
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+
+    print("2" +"asd")
+    x = "x"
+    y= "6"
+    z=int(y)+2
+    boolY =bool(y)
+    print(z)
+    UserInput = input("Enter three digits (each digit for one pig):")
+    nuberOfbrik=int(UserInput[0])+int(UserInput[1])+int(UserInput[2])
+    print(nuberOfbrik)
+    print(int(nuberOfbrik/3))
+    print(nuberOfbrik%3)
+
+print('"Shuffle, Shuffle, Shuffle"'+", say it together!\nChange colors and directions,\nDon't back down and stop the player!\tDo you want to play Taki?)        /tPress y\\")
